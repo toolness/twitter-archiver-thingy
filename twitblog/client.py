eea55451276b23a557a8b057243181bf3e8084f5
Atul Varma <varmaa@gmail.com>
2017-12-30 20:19:12 -0500

    Add original_json prop to Tweet.

--- twitblog/client.py
+++ twitblog/client.py
@@ -8,10 +8,12 @@ class Tweet:
     def __init__(self,
                  text: str,
                  screen_name: str,
-                 in_reply_to: Optional[str]) -> None:
+                 in_reply_to: Optional[str],
+                 original_json: Optional[Any]) -> None:
         self.text = text
         self.screen_name = screen_name
         self.in_reply_to = in_reply_to
+        self.original_json = original_json
 
     @classmethod
     def from_json(cls, blob: Any) -> 'Tweet':
@@ -19,6 +21,7 @@ class Tweet:
             text=blob['text'],
             screen_name=blob['user']['screen_name'],
             in_reply_to=blob['in_reply_to_status_id_str'],
+            original_json=blob,
         )
 
 
